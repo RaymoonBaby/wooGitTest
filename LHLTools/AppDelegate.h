43e7ae509917fb2ed157590b4fdea5f7a77fdea8
@RaymoonBaby <@1010565772@qq.com>
2018-04-28 15:18:42 +0800

    first

---
+++ LHLTools/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LHLTools
+//
+//  Created by liang on 2018/4/28.
+//  Copyright © 2018年 liang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LHLTools/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LHLTools
+//
+//  Created by liang on 2018/4/28.
+//  Copyright © 2018年 liang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
